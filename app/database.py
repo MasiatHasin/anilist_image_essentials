93e311b0417e18cc010e621864ae70ce7b4dd00a
Mashiat Hasin <masiathasin@gmail.com>
2024-05-25 18:08:29 +0600

    switching to postgresql

--- app/database.py
+++ app/database.py
@@ -8,15 +8,15 @@ load_dotenv()
 import os
 
 
-postgres_str = "{dialect}://{username}:{password}@{server}/{dbname}".format(
+""" postgres_str = "{dialect}://{username}:{password}@{server}/{dbname}".format(
     dialect=os.environ["POSTGRES_DIALECT"],
     server=os.environ["POSTGRES_SERVER"],
     dbname=os.environ["POSTGRES_DBNAME"],
     username=os.environ["POSTGRES_USERNAME"],
     password=os.environ["POSTGRES_PASSWORD"],
-)
+) """
 
-engine = create_engine(postgres_str)
+engine = create_engine(os.environ["POSTGRES_URL"])
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
